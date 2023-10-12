80176e08d7739af9821fd05b3e5add3713093d5b
shimjaedoo <woen1637@naver.com>
2023-10-12 17:24:51 +0900

    feat: User Entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  profileImage: string;
+
+  @Column()
+  tel: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  subAddress: string;
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+}
