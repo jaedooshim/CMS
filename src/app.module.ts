fe28f099a3e8c1c360fad5ec76e939f2f264bd2b
shimjaedoo <woen1637@naver.com>
2023-10-15 22:08:56 +0900

    chore

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './_common/typeorm.config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
     UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
