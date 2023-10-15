9a6427770127cd3abd0da17675c02687e79cc748
shimjaedoo <woen1637@naver.com>
2023-10-15 22:11:55 +0900

    feat: LoginDTO

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+}
