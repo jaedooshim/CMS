a7251a74f537af8141bb8d7b3e82d830bbde4989
shimjaedoo <woen1637@naver.com>
2023-10-15 22:10:41 +0900

    fix : 인터페이스

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/user.dto';
-import { IMessage } from '../_common/interfaces/IMessage';
+import { MessageInterface } from '../_common/interfaces/message.interface';
 
 @Controller('users')
 export class UserController {
@@ -9,7 +9,7 @@ export class UserController {
   /* 회원가입 */
   @Post()
   @HttpCode(201)
-  async create(@Body() userData: CreateUserDto): Promise<IMessage> {
+  async create(@Body() userData: CreateUserDto): Promise<MessageInterface> {
     return await this.userService.create(userData);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,14 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/user.dto';
-import { IMessage } from '../_common/interfaces/IMessage';
+import { MessageInterface } from '../_common/interfaces/message.interface';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
   /* 회원가입 */
-  async create(userData: CreateUserDto): Promise<IMessage> {
+  async create(userData: CreateUserDto): Promise<MessageInterface> {
     const { email, tel } = userData;
     /* 이메일 존재시 */
     const existingEmail = await this.userRepository.findOne({ where: { email } });
@@ -18,7 +18,7 @@ export class UserService {
     /* 전화번호 존재시 */
     const existingTel = await this.userRepository.findOne({ where: { tel } });
     if (existingTel) throw new HttpException('이미 등록된 전화번호입니다. 다시 한번 확인해주세요.', 403);
-    /* 비밀번호 복호화 */
+    /* 비밀번호 암호화 */
     userData.password = await bcrypt.hash(userData.password, 10);
     const newUser = this.userRepository.create(userData);
     await this.userRepository.save(newUser);
