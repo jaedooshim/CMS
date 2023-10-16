8b4e2f67b611a41108b081cb4a75660fa11bb9d1
shimjaedoo <woen1637@naver.com>
2023-10-17 05:57:09 +0900

    feat: PassportStrategy

---
+++ src/auth/guard/passport.jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { Payload } from '../../_common/interfaces/payload.interface';
+import * as process from 'process';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: process.env.ACCESS_SECRET_KEY,
+    });
+  }
+
+  async validate(payload: Payload, done: VerifiedCallback): Promise<any> {
+    const user = await this.authService.tokenValidateUser(payload);
+    if (!user) {
+      return done(new UnauthorizedException({ message: 'User dose not exist' }), false);
+    }
+    return done(null, user);
+  }
+}
