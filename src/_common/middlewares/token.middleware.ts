82385ab8d68922f82178dac79863eb15b25a901a
shimjaedoo <woen1637@naver.com>
2023-10-31 21:03:14 +0900

    test: console

--- src/_common/middlewares/token.middleware.ts
+++ src/_common/middlewares/token.middleware.ts
@@ -9,12 +9,15 @@ export class TokenMiddleware implements NestMiddleware {
   constructor(private jwtService: JwtService) {}
   async use(req: RequestInterface, res: Response, next: NextFunction) {
     const requestAccessToken = req.cookies.accessToken;
+    console.log('accessToken:', requestAccessToken);
 
     if (requestAccessToken) {
       try {
         req.user = this.jwtService.verify(requestAccessToken, { secret: process.env.ACCESS_SECRET_KEY });
+        console.log('req.user:', req.user);
         return next();
       } catch (error) {
+        console.log('jwt verify error:', error);
         res.clearCookie('access_token');
         return res.status(401).json({ message: 'Invalid token' });
       }
