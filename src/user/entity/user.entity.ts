c8d7ae48f03e12b29c0d092d0f51aa9b66808696
shimjaedoo <woen1637@naver.com>
2023-10-31 21:02:16 +0900

    feat: deletedAt

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -34,4 +34,7 @@ export class User {
 
   @UpdateDateColumn()
   updateAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
