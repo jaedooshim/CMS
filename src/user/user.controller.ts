ca5b04e2bf226e5c82e3683f0fd057e27d1e3ee7
shimjaedoo <woen1637@naver.com>
2023-10-31 21:07:58 +0900

    feat: 회원 CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post, Req, Res, Patch, UseGuards, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/user.dto';
 import { MessageInterface } from '../_common/interfaces/message.interface';
 import { User } from './entity/user.entity';
+import { RequestInterface } from '../_common/interfaces/request.interface';
+import { Response } from 'express';
+import { UpdateuserDto } from './dto/updateuser.dto';
+import { JwtAuthGuard } from '../auth/guard/auth.guard';
 
 @Controller('users')
 export class UserController {
@@ -24,5 +28,29 @@ export class UserController {
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<User> {
     return await this.userService.findOne(Number(id));
+  }
+
+  /* 회원 정보 수정 */
+  @Patch()
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(201)
+  async update(@Req() req: RequestInterface, @Res() res: Response, @Body() data: UpdateuserDto): Promise<Response> {
+    const { id } = req.user;
+    const { message, access_token } = await this.userService.update(id, data.email, data.address, data.subAddress);
+    res.cookie('access_token', access_token, { httpOnly: true });
+    return res.status(201).json({ message });
+  }
+
+  /* 회원 탈퇴 */
+  @Delete(':id')
+  @HttpCode(201)
+  @UseGuards(JwtAuthGuard)
+  async delete(@Param('id') userid: number, @Req() req: RequestInterface): Promise<MessageInterface> {
+    // const { id: requestId } = req.user;
+    // requestId가 string으로 나와서 Number로 지정
+    const requestId = Number(req.user.id);
+    console.log('userid:', userid);
+    console.log('requestId:', requestId);
+    return await this.userService.delete(userid, requestId);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,24 +1,32 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/user.dto';
 import { MessageInterface } from '../_common/interfaces/message.interface';
 import * as bcrypt from 'bcrypt';
+import { UpdateuserDto } from './dto/updateuser.dto';
+import { Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import * as process from 'process';
+import { TokenInterface } from '../_common/interfaces/token.interface';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
   /* 회원가입 */
   async create(userData: CreateUserDto): Promise<MessageInterface> {
     const { email, tel } = userData;
-    /* 이메일 존재시 */
+    // 이메일 중복검증
     const existingEmail = await this.userRepository.findOne({ where: { email } });
     if (existingEmail) throw new HttpException('이미 사용중인 이메일입니다. 새로운 이메일을 입력해주세요.', 403);
-    /* 전화번호 존재시 */
+    // 전화번호 중복검증
     const existingTel = await this.userRepository.findOne({ where: { tel } });
     if (existingTel) throw new HttpException('이미 등록된 전화번호입니다. 다시 한번 확인해주세요.', 403);
-    /* 비밀번호 암호화 */
+    // 비밀번호 암호화
     userData.password = await bcrypt.hash(userData.password, 10);
     const newUser = this.userRepository.create(userData);
     await this.userRepository.save(newUser);
@@ -36,5 +44,50 @@ export class UserService {
     const user = await this.userRepository.findOne({ where: { id } });
     if (!user) throw new HttpException('사용자를 찾을 수 없습니다.', 403);
     return user;
+  }
+
+  /* 회원 정보 수정 */
+  async update(id: number, email: string, address: string, subAddress: string): Promise<TokenInterface> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('회원이 존재하지 않습니다.');
+    }
+    if (email !== user.email) {
+      const existingEmail = await this.userRepository.findOne({ where: { email } });
+      if (existingEmail) {
+        throw new NotFoundException('이미 사용중인 이메일입니다.');
+      }
+    }
+    user.email = email;
+    user.address = address;
+    user.subAddress = subAddress;
+
+    const updateUser = await this.userRepository.save(user);
+    const payload = {
+      id: updateUser.id,
+      email: updateUser.email,
+      address: updateUser.address,
+      subAddress: updateUser.subAddress,
+    };
+    return {
+      access_token: this.jwtService.sign(payload, {
+        expiresIn: process.env.JWT_ACCESS_EXPIRATION_TIME,
+        secret: process.env.ACCESS_SECRET_KEY,
+      }),
+      message: '회원 정보가 수정되었습니다.',
+    };
+  }
+
+  /* 회원 탈퇴 */
+  async delete(userId: number, requestId: number): Promise<MessageInterface> {
+    if (userId !== requestId) {
+      throw new UnauthorizedException('권한이 없습니다.');
+    }
+    const existingUser = await this.userRepository.findOne({ where: { id: userId } });
+    if (!existingUser) {
+      throw new NotFoundException('존재하지 않는 회원입니다.');
+    }
+    await this.userRepository.softDelete(userId);
+    return { message: '정상적으로 처리되었습니다.' };
   }
 }
