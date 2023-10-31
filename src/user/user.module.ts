0a9f2e4ab79b65a8c2622c056d9f9d5d68df6bc4
shimjaedoo <woen1637@naver.com>
2023-10-31 21:01:31 +0900

    feat: middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { TokenMiddleware } from '../_common/middlewares/token.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtService],
 })
-export class UserModule {}
+export class UserModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TokenMiddleware).forRoutes(UserController);
+  }
+}
