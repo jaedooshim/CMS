65d24166704947787f0d7f2b3333a20c389e0c70
shimjaedoo <woen1637@naver.com>
2023-10-31 21:05:14 +0900

    fix: strategy 수정

--- src/auth/guard/passport.jwt.strategy.ts
+++ src/auth/guard/passport.jwt.strategy.ts
@@ -1,12 +1,35 @@
+// import { Injectable, UnauthorizedException } from '@nestjs/common';
+// import { PassportStrategy } from '@nestjs/passport';
+// import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+// import { AuthService } from '../auth.service';
+// import { Payload } from '../../_common/interfaces/payload.interface';
+// import * as process from 'process';
+//
+// @Injectable()
+// export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+//   constructor(private authService: AuthService) {
+//     super({
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       ignoreExpiration: true,
+//       secretOrKey: process.env.ACCESS_SECRET_KEY,
+//     });
+//   }
+//
+//   async validate(payload: Payload, done: VerifiedCallback): Promise<any> {
+//     const user = await this.authService.tokenValidateUser(payload);
+//     if (!user) {
+//       return done(new UnauthorizedException({ message: 'User dose not exist' }), false);
+//     }
+//     return done(null, user);
+//   }
+// }
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from '../auth.service';
-import { Payload } from '../../_common/interfaces/payload.interface';
-import * as process from 'process';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -15,11 +38,11 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: Payload, done: VerifiedCallback): Promise<any> {
+  async validate(payload: any) {
     const user = await this.authService.tokenValidateUser(payload);
     if (!user) {
-      return done(new UnauthorizedException({ message: 'User dose not exist' }), false);
+      throw new UnauthorizedException();
     }
-    return done(null, user);
+    return user;
   }
 }
