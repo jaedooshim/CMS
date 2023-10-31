304a6517ed782b01ee667eed3ddfb3fbbc70cbfa
shimjaedoo <woen1637@naver.com>
2023-10-31 21:06:27 +0900

    fix: 가드 JWT 변경

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -1,13 +1,25 @@
-import { CanActivate, Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+// import { CanActivate, Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+//
+// @Injectable()
+// export class AuthGuard implements CanActivate {
+//   constructor() {}
+//
+//   async canActivate(context: ExecutionContext): Promise<boolean> {
+//     const request = context.switchToHttp().getRequest();
+//     if (!request.user) throw new UnauthorizedException('로그인이 필요합니다.');
+//
+//     return true;
+//   }
+// }
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-  constructor() {}
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    if (!request.user) throw new UnauthorizedException('로그인이 필요합니다.');
-
-    return true;
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException('로그인이 필요합니다.');
+    }
+    return user;
   }
 }
